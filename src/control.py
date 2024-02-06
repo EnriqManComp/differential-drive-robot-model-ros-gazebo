d3ee50a3362bc787b90a356c0a7dc8c594df9fd0
EnriqMan <companioni@cicese.edu.mx>
2024-02-05 22:59:32 -0800

    Created main script, fix control.py code, topics:/controls, /laser_data

--- src/control.py
+++ src/control.py
@@ -2,150 +2,42 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-import threading
-
-import roslib
-import sys, select, termios, tty
-msg= """
-Reading from the keyboard  and Publishing to Twist!
+from std_msgs.msg import String
 
 
+twist_msg = Twist()
 
-"""
+control = {
+    'w': (0.2,0.0)
+}
 
-moveBindings = {
-        "w": (1,0,0,0),
-        "s": (-1,0,0,0),
-        "a": (0,0,0,1),
-        "d": (0,0,0,-1),        
-    }
+def shutdown_hook():
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    twist_msg.linear.x = 0.0 # Stop linear movement
+    twist_msg.angular.z = 0.0 # Stop angular movement
+    print('Shutting Down ...')
+    pub.publish(twist_msg)
 
+def controls_callback(msg):
+    (x,z) = control[msg.data]
+    twist_msg.linear.x = x # Modify linear velocity
+    twist_msg.angular.z = z # Modify angular velocity
+    print('Velocities:['+str(x)+','+str(z)+']')
+def controls():
+    rospy.init_node('cmd_vel_publisher', anonymous=True)
+    rospy.Subscriber('/controls',String,controls_callback)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rospy.on_shutdown(shutdown_hook) # Shut down callback function
+    rate = rospy.Rate(10) # message publish frequency
+    while not rospy.is_shutdown():        
+        print('Listening ...')
+        pub.publish(twist_msg)
+        rate.sleep()
+    rospy.spin()
 
-
-class Control(threading.Thread):
-    def __init__(self, rate):
-        super(Control, self).__init__()
-        self.publisher = rospy.Publisher("cmd_vel", Twist, queue_size=1)
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.th = 0.0
-        self.speed = 0.0
-        self.turn = 0.0
-        self.done = False
-
-
-        self.condition = threading.Condition()
-
-        # Set timeout to None if rate is 0
-        if rate != 0.0:
-            self.timeout = 1.0 / rate
-        else:
-            self.timeout = None
-
-        self.start()
-
-    def update(self, x, y, th, speed, turn):
-        self.condition.acquire()
-        
-        self.x = x        
-        self.y = y        
-        self.th = th
-        self.speed = speed
-        self.turn = turn
-        
-        # Notify publish thread that we have a new message
-        self.condition.notify()
-        self.condition.release()
-
-    def stop(self):
-        self.done = True
-        self.update(0, 0, 0, 0, 0)
-        self.join()
-
-    def run(self):
-        twist = Twist()
-        while not self.done:
-            self.condition.acquire()
-            # Wait for a new message or timeout
-            self.condition.wait(self.timeout)
-            # Copy state into twist message
-            twist.linear.x = (self.x * self.speed) 
-            twist.linear.y = (self.y * self.speed) 
-            twist.linear.z = 0.0
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
-            twist.angular.z = (self.th * self.turn) 
-
-            
-
-            self.condition.release()
-
-            # Publish update
-            self.publisher.publish(twist)
-
-        # Publish stop message when thread exits
-        twist.linear.x = 0.0
-        twist.linear.y = 0.0
-        twist.linear.z = 0.0
-        twist.angular.x = 0.0
-        twist.angular.y = 0.0
-        twist.angular.z = 0.0
-        self.publisher.publish(twist)
-
-def getKey(key_timeout):
-    tty.setraw(sys.stdin.fileno())
-    rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
-    if rlist:
-        key = sys.stdin.read(1)
-    else:
-        key = ''
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
-
-if __name__=="__main__":
-    settings = termios.tcgetattr(sys.stdin)
-    rospy.init_node("control_robot_node")    
-
-    speed = rospy.get_param("~speed", 0.2)
-    turn = rospy.get_param("~turn", 0.2)
-    repeat = rospy.get_param("~repeat_rate", 0.0)
-    key_timeout = rospy.get_param("~key_timeout", 0.0)
-    if key_timeout == 0.0:
-        key_timeout = None
-
-    pub_thread = Control(repeat)
-    
-    x = 0
-    y = 0    
-    th = 0
-    status = 0
-
+if __name__ == '__main__':
     try:
-        pub_thread.update(x, y, th, speed, turn)
-        while(1):
-            key = getKey(key_timeout)
-            if key in moveBindings.keys():
-                x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                th = moveBindings[key][3]
-            else:
-                # Skip updating cmd_vel if key timeout and robot already stopped
-                if key == '' and x == 0 and y == 0 and th == 0:
-                    continue
-                x = 0
-                y = 0
-                th = 0
-                if key == '\x03':
-                    break
-            
-            pub_thread.update(x, y, th, speed, turn)
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        pub_thread.stop()
-
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-
+        controls()
+    
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/main_script.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import String
+
+class MainScript:
+    def __init__(self):
+        self.laser_data_sub = rospy.Subscriber('/laser_data', Float32MultiArray, self.control_callback)
+        self.pub = rospy.Publisher("/controls", String, queue_size=10)
+        self.laser_data = None
+        self.control_order = String()
+        self.control = 'w'
+        self.control_order.data = self.control
+
+    def control_callback(self, data):
+        self.laser_data = data
+
+    def print_data(self):
+        print(self.laser_data)
+
+
+if __name__=="__main__":
+    rospy.init_node("Main_script")   
+    main_script = MainScript()
+         
+    rate = rospy.Rate(10) # Frecuencia de publicación del mensaje
+    while not rospy.is_shutdown():        
+        print('Escuchando')
+        main_script.print_data()    
+        main_script.pub.publish(main_script.control_order)
+        rate.sleep()
+    rospy.spin()
+    
+
+
+
+        
